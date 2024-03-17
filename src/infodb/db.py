2edbe51dbbfc85c07b897116807e181125333176
shubh <shubhvjain@gmail.com>
2024-03-17 16:07:35 +0100

    insert and get method

--- src/infodb/db.py
+++ src/infodb/db.py
@@ -1,32 +1,77 @@
 
 import couchdb
+from . import util as ut
+from jsonschema import validate
+
 
 class db:
-    def __init__(self, url, dbname):
+    def __init__(self, url, db_name, schema_name, schema_json_string):
         self.url = url
-        self.dbname = dbname
+        self.dbName = db_name
+        self.schemaName = schema_name
+        self.schema = ut.process_schema(schema_json_string)
         self.server = couchdb.Server(url)
-        self.db = self.server[dbname]
+        self.db = self.server[db_name]
 
+    def get_single_doc(self,search_criteria):
+        try:
+            doc_obj = {"schema": { "$eq": self.schemaName }}
+            for k in self.schema["primary_key"]:
+                doc_obj["data."+k] = { "$eq": search_criteria[k] }
+            doc_check = {"selector": doc_obj}
+            docs_found = self.db.find(doc_check)
+            docs_test = []
+            for r in docs_found:
+                docs_test.append(dict(r))
+            if len(docs_test) > 0:
+                the_doc = docs_test[0]
+                return the_doc
+            else :
+                return None 
+        except Exception as e:
+            raise e    
+    
     def get(self, doc_id):
         try:
             doc = self.db[doc_id]
-            return doc
+            if doc["schema"] != self.schemaName:
+                raise Exception("Not found")
+            return dict(doc)
         except couchdb.ResourceNotFound:
-            print("Document not found")
             return None
 
+    def get_using_key(self,criteria):
+        try:
+            doc_search = self.get_single_doc(criteria)    
+            if doc_search["schema"] != self.schemaName:
+                raise Exception("Not found")
+            return doc_search
+        except couchdb.ResourceNotFound:
+            return None        
+
+    def check_if_doc_exists(self,data):
+        try:
+            doc_search = self.get_single_doc(data)
+            if doc_search is not None :
+                raise Exception("Document already exists. Insert failed")
+        except Exception as e:
+            raise e
+    
     def insert(self, data):
         try:
-            doc_id, _ = self.db.save(data)
-            print(f"Document inserted with id: {doc_id}")
-            return doc_id
+            validate(data,self.schema)
+            self.check_if_doc_exists(data)
+            new_doc = ut.generate_blank_doc(self.schemaName,self.schema)
+            new_doc["data"] = data
+            self.db.save(new_doc)
+            return new_doc
         except Exception as e:
             print(f"Error inserting document: {e}")
             return None
 
     def update(self, doc_id, data):
         try:
+            # first check if the schema matches
             doc = self.db[doc_id]
             data['_id'] = doc_id
             data['_rev'] = doc['_rev']
@@ -39,6 +84,7 @@ class db:
 
     def delete(self, doc_id):
         try:
+            # first check if schema matches
             doc = self.db[doc_id]
             self.db.delete(doc)
             print(f"Document with id {doc_id} deleted")
---
+++ src/infodb/util.py
@@ -0,0 +1,65 @@
+import json
+from jsonschema import validate
+import time
+from uuid import uuid4
+import datetime
+
+def find_keys_missing(keys_to_check,data):
+  missing = []
+  for key in keys_to_check:
+    if key not in data:
+      missing.append(key)
+  return missing
+
+def process_schema(schema_string):
+  try:
+      valid_schema_doc = {
+         "type":"object",
+         "properties":{
+            "title":{"type":"string"},
+            "description":{"type":"string"},
+            "type":{"type":"string"},
+            "properties":{"type":"object"},
+            "primary_key":{"type":"array","items":{"type":"string"},"uniqueItems": True},
+            "update_allowed":{"type":"array","items":{"type":"string"},"uniqueItems": True}
+         },
+         "required":["title","description","properties","primary_key","update_allowed"]
+      }
+      data = json.loads(schema_string)
+      data["type"]="object"
+      validate(data,valid_schema_doc)
+      
+      required_keys = ["title","description","type","properties","primary_key","update_allowed"]
+      missing_required_keys = find_keys_missing(required_keys,data)
+      if len(missing_required_keys)>0:
+         raise Exception("Invalid schema. Following fields missing: "+str(missing_required_keys))
+      """
+      check if primary key and updated_allowed has a intersection
+      all primary keys must be of they string 
+      """
+
+      return data
+  except Exception as e:
+     print("Error")
+     raise e
+
+def get_current_unix_timestamp():
+  return int(time.time())
+
+def get_current_timestamp_human():
+  c_ts = get_current_unix_timestamp()
+  return datetime.datetime.fromtimestamp(c_ts).strftime('%Y-%m-%d %H:%M:%S %Z') 
+
+def generate_blank_doc(schema_name,schema):
+  c_ts = get_current_unix_timestamp()
+  new_doc = {
+      '_id': uuid4().hex,
+      "data":{},
+      "meta":{
+         "created_on":c_ts,
+         "tags":[],
+         "note":"Created on :  " + get_current_timestamp_human()
+      },
+      "schema":schema_name,
+   }
+  return new_doc
